7ca7b17bd442eace9766d0ba673e8c918ed80024
juanvelozo <juanvelozomoreno18@gmail.com>
2020-12-10 15:53:47 -0300

    update

--- js/index.js
+++ js/index.js
@@ -9,18 +9,18 @@ window.sr = ScrollReveal();
         delay: 600,
         duration: 2700,
         origin: 'left',
-        distance: '-200px'
+        distance: '-100px'
     });
     sr.reveal('.gallery-img__2', {
-        delay: 1400,
+        delay: 600,
         origin: 'top',
-        distance: '-200px',
+        distance: '-100px',
         duration: 2700,
 
     });
     sr.reveal('.gallery-img__3', {
-        delay: 900,
+        delay: 600,
         origin: 'right',
-        distance: '-200px',
+        distance: '-100px',
         duration: 2700,
     });

a14aa493f3fa542c58b9ea15aef250c5120b4892
juanvelozo <juanvelozomoreno18@gmail.com>
2020-12-13 23:55:29 -0300

    commit on gh-pages

--- js/index.js
+++ js/index.js
@@ -4,7 +4,41 @@ window.sr = ScrollReveal();
         origin: 'top',
         distance: '-100px'
     });
-
+    sr.reveal('.experience-text', {
+        delay:300,
+        duration: 1300,
+        origin: 'top',
+        distance: '-100px'
+    });
+    sr.reveal('.experience-img__1', {
+        delay: 600,
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.experience-img__2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+    sr.reveal('.experience-img__3', {
+        delay: 600,
+        duration: 2000,
+        origin: 'left',
+        distance: '-100px'
+    });
+    sr.reveal('.experience-img__4', {
+        delay: 600,
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.experience-img__5', {
+        delay: 600,
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
     sr.reveal('.gallery', {
         delay:300,
         duration: 2700,
